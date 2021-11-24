811833350d19ad0e44e6858dd0251ce4c6659a43
sofiaversoza <sofversoza@gmail.com>
2021-11-24 11:18:44 -0700

    commit tree 70f3207973aba121e02320ba41aebf8e82326436

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase(); 
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'lola';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'me';
+}
